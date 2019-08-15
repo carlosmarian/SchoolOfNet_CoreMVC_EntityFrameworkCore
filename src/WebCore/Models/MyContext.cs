4d14b09731b22759c24f5b4b53f9f317b7da385c
Carlos H. Marian <carlos.marian@amcom.com.br>
2019-08-15 20:24:32 -0300

    Ajuste nos models

---
+++ src/WebCore/Models/MyContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebCore.Models.ManageBlog;
+
+namespace WebCore.Models
+{
+    public class MyContext : DbContext
+    {
+        public DbSet<Blog> Blog { get; set;  }
+        public DbSet<Post> Post { get; set; }
+    }
+}
